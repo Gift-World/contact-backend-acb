4ab3592e410bb8ce56fec3c832da6c8483b91570
Charles <charlesgiftangila@gmail.com>
2025-03-31 01:54:47 +0300

    change server port from 3002 to 3001

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.post('/send-email', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
